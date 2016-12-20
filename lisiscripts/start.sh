01a3872c3a8d981b84db67bbd3bf768f7d7405a9
angbrav <ubuntu@ip-172-31-32-80.eu-west-1.compute.internal>
2016-12-20 18:58:24 +0000

    modified start

--- lisiscripts/start.sh
+++ lisiscripts/start.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-Command0="cd ./li_si && sudo ./rel/antidote/bin/antidote stop"
+Command0="cd ./li-si && sudo ./rel/antidote/bin/antidote stop"
 
-./lisiscripts/parallel_command_all.sh "$Command0"
+./lisiscripts/parallel_command.sh nodes "$Command0"
 
 if [ $# -eq 1 ]
 then
     branch=$1
-    Command1="cd ./li_si && git reset --hard && git fetch && git checkout $branch && git pull"
+    Command1="cd ./li-si && git reset --hard && git fetch && git checkout $branch && git pull"
 else
-    Command1="cd ./li_si && git pull origin master"
+    Command1="cd ./li-si && git pull origin master"
 fi
 
-./lisiscripts/parallel_command_all.sh "$Command1"
+./lisiscripts/parallel_command.sh nodes "$Command1"
 
 ./lisiscripts/change_nodename.sh nodes
 
-Command3="cd ./li_si && sudo make relclean"
+Command3="cd ./li-si && sudo make relclean"
 
-./lisiscripts/parallel_command_all.sh "$Command3"
+./lisiscripts/parallel_command.sh nodes "$Command3"
 
-Command4="cd ./li_si && make rel"
+Command4="cd ./li-si && make rel"
 
-./lisiscripts/parallel_command_all.sh "$Command4"
+./lisiscripts/parallel_command.sh nodes "$Command4"
 
-Command5="cd ./li_si && sudo ./rel/antidote/bin/antidote start"
+Command5="cd ./li-si && sudo ./rel/antidote/bin/antidote start"
 
-./lisiscripts/parallel_command_all.sh "$Command5"
+./lisiscripts/parallel_command.sh nodes "$Command5"
