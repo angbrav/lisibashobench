5c4e2cd41fb3b248de127a4ff80496a19d2185ff
angbrav <ubuntu@ip-172-31-27-197.eu-west-1.compute.internal>
2016-12-21 12:26:25 +0000

    Fix bugs

--- lisiscripts/change_nodename.sh
+++ lisiscripts/change_nodename.sh
@@ -10,8 +10,8 @@ for node in $nodes
 do
     let Counter=Counter+1
     NodeName="$Type$Counter@$node"
-    Command2="cd ./li_si && sudo sed -i -e \"s/{node.*/{node, '$NodeName'}./\" rel/vars.config"
-    ssh -o ConnectTimeout=10 -t ubuntu@$node -i /Users/bravogestoso/Projects/ec2-saturn ${Command2/localhost/$node} &
+    Command2="cd ./li-si && sudo sed -i -e \"s/{node.*/{node, '$NodeName'}./\" rel/vars.config"
+    ssh -o ConnectTimeout=10 -t ubuntu@$node -i ~/ec2-saturn ${Command2/localhost/$node} &
 done
 echo $Command2 done
 
--- lisiscripts/joinNodes.sh
+++ lisiscripts/joinNodes.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
 
-if [ $# -eq 1 ]
-then
-    First=`head -1 ./lisiscripts/nodes`
-    Others=`awk -v var="$1" 'NR>=2&&NR<=var' ./lisiscripts/nodes`
-    echo Parameter:$1 , joinig $Others to $First
-else
-    First=$1
-    Others=$2
-    echo "Joining "$Others" to "$First
-fi
+First=`head -1 ./lisiscripts/nodes`
+Others=`tail -n +2 ./lisiscripts/nodes`
+echo joinig $Others to $First
 
-Join="sudo li-si/rel/antidote/bin/antidote-admin cluster join antidote@$First"
+Join="sudo li-si/rel/antidote/bin/antidote-admin cluster join nodes1@$First"
 PlanAndCommit="sudo li-si/rel/antidote/bin/antidote-admin cluster plan && sudo li-si/rel/antidote/bin/antidote-admin cluster commit"
 for Node in $Others
 do
-	ssh -o ConnectTimeout=10 -t root@$Node -i /Users/bravogestoso/Projects/ec2-saturn $Join
+	ssh -o ConnectTimeout=10 -t ubuntu@$Node -i ~/ec2-saturn $Join
     	sleep 1
 done
-ssh -o ConnectTimeout=10 -t root@$First -i /Users/bravogestoso/Projects/ec2-saturn $PlanAndCommit
+ssh -o ConnectTimeout=10 -t ubuntu@$First -i ~/ec2-saturn $PlanAndCommit
