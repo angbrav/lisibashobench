1f5f1b8aa903e68cf27f941e2a4b7f97774a206f
angbrav <ubuntu@ip-172-31-36-46.eu-west-1.compute.internal>
2016-12-27 16:24:42 +0000

    fix bug

--- lisiscripts/increase_load.sh
+++ lisiscripts/increase_load.sh
@@ -4,7 +4,7 @@ clients="1 2 3 4 5 6 7 8 9 10"
 config=`cat ./lisiscripts/config`
 for client in $clients;
 do
-    ./lisiscripts/conf_bench.sh 120 $config 10 10000 uniform_int 0 0 1 0 0 5
+    ./lisiscripts/conf_bench.sh 120 $client 10 10000 uniform_int 0 0 1 0 0 5
 
     Command1="sudo /usr/sbin/ntpdate -b ntp.ubuntu.com"
     ./lisiscripts/parallel_command.sh nodes "$Command1"
