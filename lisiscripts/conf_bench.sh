7b8da17468aaecfa3526a2bfb7dd2136d383e959
angbrav <ubuntu@ip-172-31-36-46.eu-west-1.compute.internal>
2016-12-27 16:49:20 +0000

    Expose skewing param

--- lisiscripts/conf_bench.sh
+++ lisiscripts/conf_bench.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 
-#Duration=$1 (seconds)
-#Concurrent=$2
-#Value=$3
-#Keys=$4
-#Distribution=$5
-#{operations, [{read_txn, $6}, {update_txn, $7}, {read_update_txn, $8}]}.
-#key_per_read_tx, $9
-#key_per_update_tx, $10
-#key_per_read_update_tx, $11
-
 Duration=$1
 Clients=$2
 Value=$3
 Keys=$4
 Distribution=$5
-ReadTx=$6
-UpdateTx=$7
-Tx=$8
-KRTx=$9
-KUTx=${10}
-KTx=${11}
+NP=$6
+Rate=$7
+KRTx=$8
+KUTx=$9
+KTx=${10}
 
 ./lisiscripts/set_bench_param.sh "{duration.*/{duration, $Duration}"
 ./lisiscripts/set_bench_param.sh "{concurrent.*/{concurrent, $Clients}"
 ./lisiscripts/set_bench_param.sh "{value_generator.*/{value_generator, {fixed_bin, $Value}}"
 ./lisiscripts/set_bench_param.sh "{key_generator.*/{key_generator, {$Distribution, $Keys}}"
-./lisiscripts/set_bench_param.sh "{operations.*/{operations, [{read_update_txn, $Tx}]}"
+./lisiscripts/set_bench_param.sh "{num_partitions.*/{num_partitions, $NP}"
+./lisiscripts/set_bench_param.sh "{skewed_part_rate.*/{skewed_part_rate, $Rate}"
 ./lisiscripts/set_bench_param.sh "{key_per_read_tx.*/{key_per_read_tx, $KRTx}"
 ./lisiscripts/set_bench_param.sh "{key_per_update_tx.*/{key_per_update_tx, $KUTx}"
 ./lisiscripts/set_bench_param.sh "{key_per_read_update_tx.*/{key_per_read_update_tx, $KTx}"
