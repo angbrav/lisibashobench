3472e9df932cc3a6647ca568375d2842297bc17a
marsleezm <mars.leezm@gmail.com>
2017-01-16 13:42:56 +0000

    Change order of config

--- lisiscripts/run_clock_bench.sh
+++ lisiscripts/run_clock_bench.sh
@@ -11,11 +11,11 @@ do
         for SR in $StartRate:
         do
             ./lisiscripts/restart_new_clock.sh $Clock 
-            ./lisiscripts/conf_bench.sh 120 7 10 10000 uniform_int 8 $PR $SR 1 0 0 5
             ./lisiscripts/init_bench.sh ec2
+            ./lisiscripts/conf_bench.sh 60 7 10 10000 uniform_int 8 $PR $SR 1 0 0 5
             ./lisiscripts/run_bench.sh
             ./lisiscripts/summary.sh
-            echo "$Clock 120 7 10 10000 uniform_int 8 $PR $SR 1 0 0 5" > ./results/current/config
+            echo "$Clock 60 7 10 10000 uniform_int 8 $PR $SR 1 0 0 5" > ./results/current/config
         done
     done
 done
