8eefeec9e2fec400570ac4bfb544102bbc11ffe6
Zhongmiao Li <mars.leezm@gmail.com>
2017-01-18 16:05:30 +0000

    Add test clock skew

--- lisiscripts/test_clock_skew.sh
+++ lisiscripts/test_clock_skew.sh
@@ -3,15 +3,15 @@
 
 tts="0 30 60 120 240 480 960"
 config=`cat ./lisiscripts/config`
-for ts in $tts;
+for ts in $tts
 do
     Command1="sudo /usr/sbin/ntpdate -b ntp.ubuntu.com"
     ./lisiscripts/parallel_command.sh nodes "$Command1"
-    sleep $tts
+    sleep $ts
 
-    ./lisiscripts/conf_bench.sh 60 10 10 10000 uniform_int 0 0 1 0 0 5
+    ./lisiscripts/conf_bench.sh 60 7 10 10000 uniform_int 8 13 13 1 0 0 5
     Command2="cd ./basho_bench && sudo ./basho_bench examples/$config"
     ./lisiscripts/parallel_command.sh clients "$Command2"
-    ./lisiscripts/gather_results.sh
-
+    ./lisiscripts/summary.sh
+    echo $ts > ./results/current/config
 done
