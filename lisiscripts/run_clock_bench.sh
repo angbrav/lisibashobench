528be5ff9916abbd94b38eec61fa4b455bd21faa
angbrav <ubuntu@ip-10-93-151-134.eu-west-1.compute.internal>
2017-06-20 22:20:50 +0000

    Change nodes and clients

--- lisiscripts/run_clock_bench.sh
+++ lisiscripts/run_clock_bench.sh
@@ -5,7 +5,7 @@ PartRate="1 10 100 500"
 ## The probability (divided by 1000) of a transaction to start in a particular partition.
 StartRate="1 10 100 500"
 #Clocks="logical aggr_logical physical"
-Clocks="aggr_logical"
+Clocks="logical physical"
 Num="1"
 
 IfPrecise=false
@@ -30,7 +30,7 @@ do
     done
 done
 
-Clocks="logical aggr_logical"
+Clocks="logical"
 IfPrecise=true
 for Clock in $Clocks
 do
@@ -41,10 +41,10 @@ do
             ./lisiscripts/restart_new_clock.sh $Clock $IfPrecise 
             for num in $Num
             do
-                ./lisiscripts/conf_bench.sh 60 7 10 10000 uniform_int 8 $PR $SR 0 0 10 2 
+                ./lisiscripts/conf_bench.sh 60 7 10 10000 uniform_int 8 $PR $SR 0 0 5 2 
                 ./lisiscripts/run_bench.sh
                 ./lisiscripts/summary.sh
-                echo "$Clock $IfPrecise 60 7 10 10000 uniform_int 8 $PR $SR 0 0 10 2" > ./results/current/config
+                echo "$Clock $IfPrecise 60 7 10 10000 uniform_int 8 $PR $SR 0 0 5 2" > ./results/current/config
             done
         done
     done
